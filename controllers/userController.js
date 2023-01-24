0499bd1d15a50e4f9b992c1ca375188074b90a9c
KumarKelashMeghwar <kumarkelash875@gmail.com>
2023-01-24 15:28:09 +0500

    some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,17 @@
 const RegistrationModel = require("../Models/RegistrationModel");
 const config = require("../config/config");
-const Model = config.Sequelize.Model;
+const bcrypt = require("bcryptjs");
 const sequelize = config.sequelizeTZ;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+//insert
 const insertData = async (req, res) => {
   try {
+    const data = req.body;
+    data.password = await bcrypt.hash(data.password, 10);
     await sequelize.sync({ force: false });
-    let insert = await RegistrationModel.create({
-      fname: "Totaram",
-      lname: "Meghwar",
-      phoneNumber: "03313171143",
-      email: "totataram@gmail.com",
-      password: "ramrama",
-    });
+    let insert = await RegistrationModel.create(data);
     res.status(200).json(insert);
   } catch (e) {
     let message = e.errors[0]["message"];
@@ -24,18 +21,13 @@ const insertData = async (req, res) => {
 
 //update
 const updateData = async (req, res) => {
+  const data = req.body;
   try {
-    let update = await RegistrationModel.update(
-      {
-        fname: "RAM",
-        lname: "KUMAR",
+    let update = await RegistrationModel.update(data, {
+      where: {
+        fname: data.fname,
       },
-      {
-        where: {
-          fname: "Totaram",
-        },
-      }
-    );
+    });
     res.status(200).json(update);
   } catch (e) {
     res.status(200).json(e);
@@ -44,18 +36,10 @@ const updateData = async (req, res) => {
 
 //select
 const selectData = async (req, res) => {
+  // const data = req.body
   try {
-    let data = await RegistrationModel.findAll(
-      {
-        attributes: ["fname", "lname", "email"],
-      },
-      {
-        where: {
-          fname: "RAM",
-        },
-      }
-    );
-    res.status(200).json(data);
+    let result = await RegistrationModel.findAll();
+    res.status(200).json(result);
   } catch (e) {
     res.status(200).json(e);
   }
@@ -63,15 +47,49 @@ const selectData = async (req, res) => {
 
 //delete
 const deleteData = async (req, res) => {
+  const data = req.body;
   try {
     let result = await RegistrationModel.destroy({
-      where: {
-        fname: "Totaram",
-      },
+      where: data,
     });
     res.status(200).json(result);
   } catch (e) {
     res.status(200).json(e);
+  }
+};
+const login = async (req, resp) => {
+  try {
+    if (!req.cookies.jwt_token) {
+      const user = req.body;
+      Userpassword = await bcrypt.hash(user.password, 10);
+      let result = await RegistrationModel.findOne({
+        where: {
+          email: user.email,
+          password: Userpassword,
+        },
+      });
+      if (!result) {
+        resp.status(404).json({ Message: "User does not exist" });
+      }
+      //   const isPasswordValid = await bcrypt.compare(user.password,foundUser.password);
+      if (result) {
+        const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+          expiresIn: "5h",
+        });
+        // resp.cookie("jwt_token",{user,token:token},{
+        //     maxAge: 60*60*5,
+        //     expires: new Date(),
+        // });
+
+        resp.cookie("jwt_token", { user, token: token });
+        resp.status(404).json({ Token: token });
+      }
+      resp.status(200).json(result);
+    }
+
+    resp.status(202).json({ message: "Already logged in" });
+  } catch (e) {
+    resp.status(402).json(e);
   }
 };
 module.exports = {
@@ -79,4 +97,5 @@ module.exports = {
   deleteData,
   updateData,
   selectData,
+  login,
 };
