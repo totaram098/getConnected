6a5aa55af63c5f0b29a7fa4d18eb7a400b083a24
KumarKelashMeghwar <kumarkelash875@gmail.com>
2023-01-24 16:37:30 +0500

    uploads

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const config = require("./config/config");
 const cookieParser = require("cookie-parser");
 const sequelizeTZ = config.sequelizeTZ;
 const auth = require("./auth/auth");
+require("dotenv").config();
 
-const port = 3001;
+const userController = require("./controllers/userController");
+const BookingController = require("./controllers/BookingController");
+
+const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const userController = require("./controllers/userController");
-const BookingController = require("./controllers/BookingController");
-app.post("/insert", userController.insertData);
-app.delete("/delete", userController.deleteData);
-app.put("/update", userController.updateData);
-app.get("/select", userController.selectData);
+const { insertData, deleteData, updateData, selectData } = userController;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to booking app!");
+});
+
+app.post("/insert", insertData);
+app.delete("/delete", deleteData);
+app.put("/update", updateData);
+app.get("/select", selectData);
 
 app.get("/profile", auth.tokenVerify, (req, resp) => {
   resp.status(202).json({ message: "Verified Now" });
@@ -22,19 +30,6 @@ app.get("/profile", auth.tokenVerify, (req, resp) => {
 
 app.post("/booking", BookingController.insertBooking);
 
-app.get("/insert", userController.insertData);
-app.get("/delete", userController.deleteData);
-app.post("/update", userController.updateData);
-app.get("/select", userController.selectData);
-// (req,res)=>{
-//     try {
-//         sequelizeTZ.authenticate();
-//         console.log('Connection has been established successfully.');
-//         res.send("successfully connected")
-//       } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//       }
-// })
 app.listen(port, () => {
-  console.log("Connected");
+  console.log("Server is running on port " + port);
 });
